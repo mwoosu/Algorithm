0902ca82504a8e8a5d4b50c74a8170cc18ecd43c
우수 <125941716+mwoosu@users.noreply.github.com>
2023-07-25 00:18:09 +0900

    [Silver III] Title: 한국이 그리울 땐 서버에 접속하지, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
+++ 백준/Silver/9996. 한국이 그리울 땐 서버에 접속하지/한국이 그리울 땐 서버에 접속하지.cc
@@ -1,30 +1,23 @@
 #include <bits/stdc++.h>
 using namespace std;
 int t, idx;
-string pat, str, frnt, bck;
+string pat, str, pre, suf;
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
     cin >> t >> pat;
     idx = pat.find('*');
-    frnt = pat.substr(0, idx);
-    bck = pat.substr(idx + 1,pat.size());
+    pre = pat.substr(0, idx);
+    suf = pat.substr(idx + 1);
     for(int i = 0; i < t; i++){
         cin >> str;
-        string ftemp, btemp;
-        if(str.size() < frnt.size() + bck.size()){
+         if(pre.size() + suf.size() > str.size()){// 반례. ab*ab, ab
             cout << "NE" << '\n';
-            continue;
-        }
-        for(int j = 0; j < frnt.size(); j++){
-            ftemp += str[j];
-        }
-        for(int k = 0; k < bck.size(); k++){
-            btemp += str[str.size()- bck.size() + k];
-        }
-        if(frnt == ftemp && bck == btemp) cout << "DA" << '\n';
-        else cout << "NE" << '\n';
+         }else{
+             if(pre == str.substr(0, pre.size()) && suf == str.substr(str.size() - suf.size())) cout << "DA\n";
+             else cout <<"NE\n";  
+         } 
     }
     return 0;
 }
